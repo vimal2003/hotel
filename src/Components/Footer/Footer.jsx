39af650f25b1266de10fe782a54a1470dcb67d38
vimal2003 <85003088+vimal2003@users.noreply.github.com>
2023-05-18 15:29:32 +0530

    Add files via upload

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Footer() {
+  const year = new Date().getFullYear();
+
+  return (
+    <footer className="footer">
+      <h2>Beach Resort | Hotel Room Book</h2>
+      <p>
+        &copy; <span>{year}</span> BeachResort.com - All Rights Copyright
+        Reserved To Vimal
+      </p>
+    </footer>
+  );
+}
